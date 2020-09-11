7c5aa1ac7cf1102846ad3b6b918fe09b25c6a57e
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2020-09-11 17:59:35 +0800

    chore: 🔧 add devmoji

--- packages/shared-utils/src/safeEval/index.ts
+++ packages/shared-utils/src/safeEval/index.ts
@@ -5,9 +5,9 @@ import { TAnyObject } from '../types';
  * 比原生 eval 安全,比 ast 轻量的 实现
  * @param returnCode - 要 eval 后返回的结果
  * @param vars - 代码字符串需要的变量
- * @param expression - 表达式
+ * @param expression - return 前的表达式
  */
-export const safeEval = (returnCode: string, vars: TAnyObject, expression?: string) => {
+export const safeEval = (returnCode: string, vars: TAnyObject = {}, expression?: string) => {
   const varsStr = Object.entries(vars).reduce((prev, next) => {
     const [key, value] = next;
     prev += `const ${key} = ${obj2json(value)};`;
